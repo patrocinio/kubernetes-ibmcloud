23f8d8a3db015d7ea197664d3afd61fb8c0e3986
Eduardo Patrocinio <epatro@gmail.com>
2016-03-06 17:38:07 -0500

    Push SSH id to the kube-master

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -11,11 +11,11 @@ function create_master {
 # Creates the kube master
 echo "Creating kube master"
 TEMP_FILE=/tmp/create-vs.out
-yes | slcli vs create --hostname kube-master --domain $DOMAIN --cpu 1 --memory 1 --datacenter $DATACENTER --billing hourly --os CENTOS_LATEST > $TEMP_FILE
-VS_ID=`grep -o "\bid.*\b" $TEMP_FILE | head -1 | awk '{print $2}'`
+#yes | slcli vs create --hostname $KUBE_MASTER --domain $DOMAIN --cpu 1 --memory 1 --datacenter $DATACENTER --billing hourly --os CENTOS_LATEST > $TEMP_FILE
+#VS_ID=`grep -o "\bid.*\b" $TEMP_FILE | head -1 | awk '{print $2}'`
 
-echo "Waiting for virtual server $VS_ID to be ready"
-slcli vs ready $VS_ID --wait=$TIMEOUT
+echo "Waiting for virtual server $KUBE_MASTER to be ready"
+slcli vs ready $KUBE_MASTER --wait=$TIMEOUT
 }
 
 # Authenticates to SL
@@ -30,15 +30,35 @@ slcli config show
 
 # Check whether kube master exists
 TEMP_FILE=/tmp/deploy-kubernetes.out
-slcli vs list --hostname $KUBE_MASTER --domain $DOMAIN > $TEMP_FILE
+slcli vs list --hostname $KUBE_MASTER --domain $DOMAIN | grep $KUBE_MASTER > $TEMP_FILE
 COUNT=`wc $TEMP_FILE | awk '{print $1}'`
 
-# There is always an empty line
-if [ $COUNT -eq 1 ]; then
+# Determine whether to create the kube-master
+if [ $COUNT -eq 0 ]; then
   create_master
 else
   echo "kube-master already created"
 fi
+
+# Obtain the root password
+slcli vs detail $KUBE_MASTER --passwords > $TEMP_FILE
+PASSWORD=`grep root $TEMP_FILE | awk '{print $3}'`
+echo PASSWORD $PASSWORD
+
+# Obtain the IP address
+set -x
+IP_ADDRESS=`grep public_ip $TEMP_FILE | awk '{print $2}'`
+
+# Generate SSH key
+yes | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Log in to the machine
+sshpass -p $PASSWORD ssh-copy-id root@$IP_ADDRESS
+
+# Test connection
+ssh root@$IP_ADDRESS whoami
+
+
 
 
 
