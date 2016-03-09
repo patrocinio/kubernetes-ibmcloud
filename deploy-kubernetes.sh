8b0bb5f5e4e1f2bb6082c2373a7fc30274778791
Eduardo Patrocinio <epatro@gmail.com>
2016-03-09 08:36:09 -0500

    Copy /etc/hosts file

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -35,31 +35,72 @@ echo "Waiting for virtual server $1 to be ready"
 slcli vs ready $1 --wait=$TIMEOUT
 }
 
-function configure_master {
+function obtain_root_pwd {
 # Obtain the root password
-slcli vs detail $KUBE_MASTER --passwords > $TEMP_FILE
+slcli vs detail $1 --passwords > $TEMP_FILE
 PASSWORD=`grep root $TEMP_FILE | awk '{print $3}'`
 echo PASSWORD $PASSWORD
 
+}
+
+# Args $1: hostname
+function obtain_ip {
 # Obtain the IP address
+slcli vs detail $1 --passwords > $TEMP_FILE
 IP_ADDRESS=`grep public_ip $TEMP_FILE | awk '{print $2}'`
+}
+
+
+function configure_master {
+# Get kube master password
+obtain_root_pwd $KUBE_MASTER
+
+# Get master IP address
+obtain_ip $KUBE_MASTER
+MASTER_IP=$IP_ADDRESS
 
 # Generate SSH key
 yes | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # Log in to the machine
-sshpass -p $PASSWORD ssh-copy-id root@$IP_ADDRESS
+sshpass -p $PASSWORD ssh-copy-id root@$MASTER_IP
+
+# Get node IP address
+obtain_ip $KUBE_NODE
+NODE_IP=$IP_ADDRESS
 
 # Update ansible hosts file
-ANSIBLE_CONFIG=./ansible/ansible.cfg
+echo Updating ansible hosts files
 HOSTS=/tmp/ansible-hosts
 echo > $HOSTS
 echo "[kube-master]" >> $HOSTS
-echo "$IP_ADDRESS ansible_user=root" >> $HOSTS
+echo "kube-master ansible_host=$MASTER_IP ansible_user=root" >> $HOSTS
+echo "[kube-node]" >> $HOSTS
+echo "kube-node ansible_host=$NODE_IP ansible_user=root" >> $HOSTS
 
 # Execute kube-master playbook
 ansible-playbook ansible/kube-master.yaml
 }
+
+function configure_node {
+set -x
+# Get kube master password
+obtain_root_pwd $KUBE_NODE
+
+# Get master IP address
+obtain_ip $KUBE_NODE
+NODE_IP=$IP_ADDRESS
+
+# Generate SSH key
+yes | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Log in to the machine
+sshpass -p $PASSWORD ssh-copy-id root@$NODE_IP
+
+# Execute kube-master playbook
+ansible-playbook ansible/kube-node.yaml
+}
+
 
 # Authenticates to SL
 echo "[softlayer]" > ~/.softlayer
@@ -75,6 +116,7 @@ create_kube $KUBE_MASTER
 create_kube $KUBE_NODE
 
 configure_master
+configure_node
 
 
 
