2009e22d8c18afadcb9e1a3ceae8393514951126
Rick Osowski <rosowski@gmail.com>
2016-06-15 13:33:42 -0500

    First pass at looping over node deployment
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -30,7 +30,7 @@ function get_vlan_id {
 
 # Args: $1: label $2: VLAN number
 function build_vlan_arg {
-if [ -z $2 ]; then
+  if [ -z $2 ]; then
     VLAN_ARG=""
   else
      get_vlan_id $2
@@ -40,181 +40,213 @@ if [ -z $2 ]; then
 
 # Args: $1: name
 function create_server {
-# Creates the machine
-echo "Creating $1 with $CPU cpu(s) and $MEMORY GB of RAM"
-TEMP_FILE=/tmp/create-vs.out
-build_vlan_arg "--vlan-private" $PRIVATE_VLAN
-PRIVATE_ARG=$VLAN_ARG
-build_vlan_arg "--vlan-public" $PUBLIC_VLAN
-PUBLIC_ARG=$VLAN_ARG
+  # Creates the machine
+  echo "Creating $1 with $CPU cpu(s) and $MEMORY GB of RAM"
+  TEMP_FILE=/tmp/create-vs.out
+  build_vlan_arg "--vlan-private" $PRIVATE_VLAN
+  PRIVATE_ARG=$VLAN_ARG
+  build_vlan_arg "--vlan-public" $PUBLIC_VLAN
+  PUBLIC_ARG=$VLAN_ARG
 
-echo "Deploying $SERVER_MESSAGE $1"
-yes | slcli $CLI_TYPE create --hostname $1 --domain $DOMAIN $SPEC --datacenter $DATACENTER --billing hourly  $PRIVATE_ARG $PUBLIC_ARG | tee $TEMP_FILE
+  echo "Deploying $SERVER_MESSAGE $1"
+  yes | slcli $CLI_TYPE create --hostname $1 --domain $DOMAIN $SPEC --datacenter $DATACENTER --billing hourly  $PRIVATE_ARG $PUBLIC_ARG | tee $TEMP_FILE
 }
 
 # Args: $1: name
 function get_server_id {
-# Extract virtual server ID
-slcli $CLI_TYPE list --hostname $1 --domain $DOMAIN | grep $1 > $TEMP_FILE
+  # Extract virtual server ID
+  slcli $CLI_TYPE list --hostname $1 --domain $DOMAIN | grep $1 > $TEMP_FILE
 
-# Consider only the first returned result
-VS_ID=`head -1 $TEMP_FILE | awk '{print $1}'`
+  # Consider only the first returned result
+  VS_ID=`head -1 $TEMP_FILE | awk '{print $1}'`
 }
 
 # Args: $1: name
 function create_kube {
-# Check whether kube master exists
-TEMP_FILE=/tmp/deploy-kubernetes.out
-slcli $CLI_TYPE list --hostname $1 --domain $DOMAIN | grep $1 > $TEMP_FILE
-COUNT=`wc $TEMP_FILE | awk '{print $1}'`
+  # Check whether kube master exists
+  TEMP_FILE=/tmp/deploy-kubernetes.out
+  slcli $CLI_TYPE list --hostname $1 --domain $DOMAIN | grep $1 > $TEMP_FILE
+  COUNT=`wc $TEMP_FILE | awk '{print $1}'`
 
-# Determine whether to create the kube-master
-if [ $COUNT -eq 0 ]; then
-create_server $1
-else
-echo "$1 already created"
-fi
-
-get_server_id $1
-
-# Wait kube master to be ready
-while true; do
-  echo "Waiting for $SERVER_MESSAGE $1 to be ready..."
-  STATE=`slcli $CLI_TYPE detail $VS_ID | grep state | awk '{ print $2}'`
-  if [ $STATE == 'RUNNING' ]; then
-    break
+  # Determine whether to create the kube-master
+  if [ $COUNT -eq 0 ]; then
+  create_server $1
   else
-    sleep 5
+  echo "$1 already created"
   fi
-done
+
+  get_server_id $1
+
+  # Wait kube master to be ready
+  while true; do
+    echo "Waiting for $SERVER_MESSAGE $1 to be ready..."
+    STATE=`slcli $CLI_TYPE detail $VS_ID | grep state | awk '{ print $2}'`
+    if [ $STATE == 'RUNNING' ]; then
+      break
+    else
+      sleep 5
+    fi
+  done
 }
 
 # Arg $1: hostname
 function obtain_root_pwd {
-get_server_id $1
+  get_server_id $1
 
-# Obtain the root password
-slcli $CLI_TYPE detail $VS_ID --passwords > $TEMP_FILE
+  # Obtain the root password
+  slcli $CLI_TYPE detail $VS_ID --passwords > $TEMP_FILE
 
-# Remove "remote users"
-# it seems that for Ubuntu it's print $4; however, for Mac, it's print $3
-PASSWORD=`grep root $TEMP_FILE | grep -v "remote users" | awk '{print $3}'`
-echo PASSWORD $PASSWORD
+  # Remove "remote users"
+  # it seems that for Ubuntu it's print $4; however, for Mac, it's print $3
+  PASSWORD=`grep root $TEMP_FILE | grep -v "remote users" | awk '{print $4}'`
+  echo PASSWORD $PASSWORD
 }
 
 # Args $1: hostname
 function obtain_ip {
-echo Obtaining IP address for $1
-get_server_id $1
-# Obtain the IP address
-slcli $CLI_TYPE detail $VS_ID --passwords > $TEMP_FILE
+  echo Obtaining IP address for $1
+  get_server_id $1
+  # Obtain the IP address
+  slcli $CLI_TYPE detail $VS_ID --passwords > $TEMP_FILE
 
-if [ $CONNECTION  == "VPN" ]; then
-  IP_ADDRESS=`grep private_ip $TEMP_FILE | awk '{print $2}'`
-else
-  IP_ADDRESS=`grep public_ip $TEMP_FILE | awk '{print $2}'`
-fi
+  if [ $CONNECTION  == "VPN" ]; then
+    IP_ADDRESS=`grep private_ip $TEMP_FILE | awk '{print $2}'`
+  else
+    IP_ADDRESS=`grep public_ip $TEMP_FILE | awk '{print $2}'`
+  fi
 }
 
 # From the standpoint of ansible, kube-master-2 is a 'node'
-function update_hosts_file {
-# Update ansible hosts file
-echo Updating ansible hosts files
-echo > $HOSTS
-echo "[kube-master]" >> $HOSTS
-obtain_ip ${KUBE_MASTER_PREFIX}1
-MASTER1_IP=$IP_ADDRESS
-echo "kube-master-1 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
-#echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+function update_hosts_file { ##TODO##
+  # Update ansible hosts file
+  echo Updating ansible hosts files
+  echo > $HOSTS
+  echo "[kube-master]" >> $HOSTS
+  obtain_ip ${KUBE_MASTER_PREFIX}1
+  MASTER1_IP=$IP_ADDRESS
+  echo "kube-master-1 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  #echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
 
-echo "[kube-node]" >> $HOSTS
-obtain_ip "${KUBE_NODE_PREFIX}1"
-NODE1_IP=$IP_ADDRESS
-echo "kube-node-1 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
-#echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
-obtain_ip "${KUBE_NODE_PREFIX}2"
-NODE2_IP=$IP_ADDRESS
-echo "kube-node-2 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
-#echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  echo "[kube-node]" >> $HOSTS
+
+  for(( x=1; x <= ${NUM_NODES}; x++))
+  do
+    obtain_ip "${KUBE_NODE_PREFIX}${x}"
+    export NODE${x}_IP=$IP_ADDRESS
+    echo "${KUBE_NODE_PREFIX}${x} ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  done
+
+  #obtain_ip "${KUBE_NODE_PREFIX}1"
+  #NODE1_IP=$IP_ADDRESS
+  #echo "kube-node-1 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  ##echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  #obtain_ip "${KUBE_NODE_PREFIX}2"
+  #NODE2_IP=$IP_ADDRESS
+  #echo "kube-node-2 ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
+  ##echo "$IP_ADDRESS ansible_host=$IP_ADDRESS ansible_user=root" >> $HOSTS
 }
 
 #Args: $1: PASSWORD, $2: IP Address
 function set_ssh_key {
-#Remove entry from known_hosts
-ssh-keygen -R $2
+  #Remove entry from known_hosts
+  ssh-keygen -R $2
 
-# Log in to the machine
-sshpass -p $1 ssh-copy-id -o 'StrictHostKeyChecking=no' root@$2
+  # Log in to the machine
+  sshpass -p $1 ssh-copy-id -o 'StrictHostKeyChecking=no' root@$2
 }
 
 #Args: $1: master hostname $2: master IP
-function configure_master {
-# Get kube master password
-obtain_root_pwd $1
+function configure_master { ##TODO##
+  # Get kube master password
+  obtain_root_pwd $1
 
-# Set the SSH key
-set_ssh_key $PASSWORD $2
+  # Set the SSH key
+  set_ssh_key $PASSWORD $2
 
-# Create inventory file
-INVENTORY=/tmp/inventory
-echo > $INVENTORY
-echo "[masters]" >> $INVENTORY
-echo "kube-master-1" >> $INVENTORY
-echo >> $INVENTORY
-echo "[etcd]" >> $INVENTORY
-echo "kube-master-1" >> $INVENTORY
-echo >> $INVENTORY
-echo "[nodes]" >> $INVENTORY
-echo "$NODE1_IP" >> $INVENTORY
-echo "$NODE2_IP" >> $INVENTORY
+  # Create inventory file
+  INVENTORY=/tmp/inventory
+  echo > $INVENTORY
+  echo "[masters]" >> $INVENTORY
+  echo "kube-master-1" >> $INVENTORY
+  echo >> $INVENTORY
+  echo "[etcd]" >> $INVENTORY
+  echo "kube-master-1" >> $INVENTORY
+  echo >> $INVENTORY
+  echo "[nodes]" >> $INVENTORY
+  #echo "$NODE1_IP" >> $INVENTORY
+  #echo "$NODE2_IP" >> $INVENTORY
 
-# Create ansible.cfg
-ANSIBLE_CFG=/tmp/ansible.cfg
-echo > $ANSIBLE_CFG
-echo "[defaults]" >> $ANSIBLE_CFG
-echo "host_key_checking = False" >> $ANSIBLE_CFG
+  for(( x=1; x <= ${NUM_NODES}; x++))
+  do
+    TMP1=$(echo \${NODE${x}_IP})
+    LOCAL_IP=$(eval ${TMP1})
+    echo "${LOCAL_IP}" >> ${INVENTORY}
+  done
+
+  # Create ansible.cfg
+  ANSIBLE_CFG=/tmp/ansible.cfg
+  echo > $ANSIBLE_CFG
+  echo "[defaults]" >> $ANSIBLE_CFG
+  echo "host_key_checking = False" >> $ANSIBLE_CFG
 
 }
 
-function configure_masters {
+function configure_masters { ##TODO##
   configure_master ${KUBE_MASTER_PREFIX}1 $MASTER1_IP
 
+  EXTRA_VARS=""
+  for(( x=1; x <= ${NUM_NODES}; x++))
+  do
+    TMP1=$(echo \${NODE${x}_IP})
+    LOCAL_IP=$(eval ${TMP1})
+    EXTRA_VARS += "kube_node${x}=${LOCAL_IP} "
+  done
+
   # Execute kube-master playbook
-  ansible-playbook -i $HOSTS ansible/kube-master.yaml --extra-vars "kube_node1=$NODE1_IP kube_node2=$NODE2_IP"
+  #ansible-playbook -i $HOSTS ansible/kube-master.yaml --extra-vars "kube_node1=$NODE1_IP kube_node2=$NODE2_IP"
+  ansible-playbook -i $HOSTS ansible/kube-master.yaml --extra-vars "${EXTRA_VARS}"
 }
 
 # Args $1 Node name
 function configure_node {
-echo Configuring node $1
+  echo Configuring node $1
 
-# Get kube master password
-obtain_root_pwd $1
+  # Get kube master password
+  obtain_root_pwd $1
 
-# Get master IP address
-obtain_ip $1
-NODE_IP=$IP_ADDRESS
-echo IP Address: $NODE_IP
+  # Get master IP address
+  obtain_ip $1
+  NODE_IP=$IP_ADDRESS
+  echo IP Address: $NODE_IP
 
-# Set the SSH key
-set_ssh_key $PASSWORD $NODE_IP
+  # Set the SSH key
+  set_ssh_key $PASSWORD $NODE_IP
 }
 
-function configure_nodes {
-echo Configuring nodes
-configure_node "${KUBE_NODE_PREFIX}1"
-configure_node "${KUBE_NODE_PREFIX}2"
+function configure_nodes { ##TODO##
+  echo Configuring nodes
+  for(( x=1; x <= ${NUM_NODES}; x++))
+  do
+    configure_node "${KUBE_NODE_PREFIX}${x}"
+  done
+  #configure_node "${KUBE_NODE_PREFIX}1"
+  #configure_node "${KUBE_NODE_PREFIX}2"
 
-# Execute kube-master playbook
-ansible-playbook -i $HOSTS ansible/kube-node.yaml
+  # Execute kube-master playbook
+  ansible-playbook -i $HOSTS ansible/kube-node.yaml
 }
 
 function create_nodes {
-create_kube "${KUBE_NODE_PREFIX}1"
-create_kube "${KUBE_NODE_PREFIX}2"
+  for(( x=1; x <= ${NUM_NODES}; x++))
+  do
+    create_kube "${KUBE_NODE_PREFIX}${x}"
+  done
+  #create_kube "${KUBE_NODE_PREFIX}1"
+  #create_kube "${KUBE_NODE_PREFIX}2"
 }
 
 function create_masters {
-create_kube "${KUBE_MASTER_PREFIX}1"
+  create_kube "${KUBE_MASTER_PREFIX}1"
 }
 
 # Authenticates to SL
