6d3a53a69985fd96b95b6d41dc6b70b5db55cd8c
Eduardo Patrocinio <epatro@gmail.com>
2021-04-23 17:29:28 -0400

    Work on first master

--- terraform/modules/is_lb/main.tf
+++ terraform/modules/is_lb/main.tf
@@ -3,16 +3,16 @@ resource "ibm_is_lb" "is_lb" {
   name            = var.name
   subnets         = [var.subnet_id]
   resource_group  = var.resource_group
+  profile         = "network-fixed"
 }
 
 resource "ibm_is_lb_pool" "is_lb_pool" {
   name           = var.name
   lb             = ibm_is_lb.is_lb.id
   algorithm      = "round_robin"
-  protocol       = "https"
+  protocol       = "tcp"
   health_delay   = 60
   health_retries = 5
   health_timeout = 30
-  health_type    = "https"
-  proxy_protocol = "v1"
+  health_type    = "tcp"
 }
--- terraform/modules/is_lb_pool_member/main.tf
+++ terraform/modules/is_lb_pool_member/main.tf
@@ -2,6 +2,6 @@ resource "ibm_is_lb_pool_member" "testacc_lb_mem" {
   lb             =  var.lb_id
   pool           = var.lb_pool_id
   port           = 6443
-  target_address = var.masters[0].primary_network_interface[0].primary_ipv4_address
+  target_id      = var.masters[0].id
   weight         = 60
 }
