609d50b1826c10cac57815e8a30298d5a61fa4c3
Eduardo Patrocinio <epatro@gmail.com>
2021-06-05 09:54:19 -0400

    Add all master nodes to load balance

--- terraform/modules/is_lb_pool_member/main.tf
+++ terraform/modules/is_lb_pool_member/main.tf
@@ -1,7 +1,8 @@
 resource "ibm_is_lb_pool_member" "testacc_lb_mem" {
+  count          = length(var.masters)
   lb             =  var.lb_id
   pool           = var.lb_pool_id
   port           = 6443
-  target_address = var.masters[0].primary_network_interface[0].primary_ipv4_address
+  target_address = var.masters[count.index].primary_network_interface[0].primary_ipv4_address
   weight         = 60
 }
